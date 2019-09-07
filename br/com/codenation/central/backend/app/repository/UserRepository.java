6575e40f346526401ccc2b1ed833fea771158da4
Erica Peters <ericapetersc@gmail.com>
2019-09-07 13:46:49 -0300

    Add errorLevel on entity Error

--- br/com/codenation/central/backend/app/repository/UserRepository.java
+++ br/com/codenation/central/backend/app/repository/UserRepository.java
@@ -2,5 +2,10 @@ package br.com.codenation.central.backend.app.repository;
 
 import br.com.codenation.central.backend.app.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends RepositoryInterface<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
