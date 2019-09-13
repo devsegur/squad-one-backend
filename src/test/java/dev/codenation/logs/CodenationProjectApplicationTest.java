501ccbad119d79d07cd4b553e3bcb9c2d2620736
Everton Segur <evertonsegur5@gmail.com>
2019-09-13 00:26:02 -0300

    test(created single file test to configure circleci) that is the only change of this commit

---
+++ src/test/java/dev/codenation/logs/CodenationProjectApplicationTest.java
@@ -0,0 +1,22 @@
+package dev.codenation.logs;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CodenationProjectApplicationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
