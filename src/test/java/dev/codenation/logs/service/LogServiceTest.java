e8804c9c50f9ae2085f79d5f098d4e23c4cce5ba
Erica Peters <ericapetersc@gmail.com>
2019-09-16 02:17:38 -0300

    Add user service tests

--- src/test/java/dev/codenation/logs/service/LogServiceTest.java
+++ src/test/java/dev/codenation/logs/service/LogServiceTest.java
@@ -1,6 +1,11 @@
 package dev.codenation.logs.service;
 
 import dev.codenation.logs.domain.entity.Log;
+import dev.codenation.logs.domain.entity.User;
+import dev.codenation.logs.domain.enums.Environment;
+import dev.codenation.logs.domain.enums.Severity;
+import dev.codenation.logs.domain.valueObject.LogDetail;
+import dev.codenation.logs.domain.valueObject.Origin;
 import dev.codenation.logs.repository.LogRepository;
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -28,18 +33,33 @@ public class LogServiceTest {
     private LogRepository repository;
 
     @Test
-    public void abc() {
-        UUID uuid = UUID.randomUUID();
-        when(repository.findById(uuid)).thenReturn(Optional.of(getLog()));
+    public void WhenFindByValidId_LogShouldBeReturned() {
+        UUID id = UUID.randomUUID();
+        Log logExpected = createLog();
+        logExpected.setId(id);
+        when(repository.findById(id)).thenReturn(Optional.of(logExpected));
 
-        Optional<Log> result = service.findById(uuid);
+        Optional<Log> logFound = service.findById(id);
 
-        assertThat(result.get(), Matchers.notNullValue());
+        assertThat(logFound.get(), Matchers.equalTo(logExpected));
 
     }
 
-    private Log getLog() {
-        return new Log();
+    private Log createLog() {
+        return Log.builder()
+                .archived(false)
+                .hash("Hash")
+                .logDetail(LogDetail.builder()
+                        .message("Message")
+                        .details("Details")
+                        .severity(Severity.DEBUG)
+                        .build())
+                .origin(Origin.builder()
+                        .origin("localhost")
+                        .environment(Environment.DEV)
+                        .build())
+                .reportedBy(new User())
+                .build();
     }
 
 }
